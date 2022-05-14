ec4f55aa80c0ea9169416ab0d7adcb4f752a5f50
GabrielAbra <gabrielarr@al.insper.edu.br>
2022-05-14 15:13:36 -0300

    Update analysis

--- modfin/analysis_/__init__.py
+++ modfin/analysis_/__init__.py
@@ -3,8 +3,8 @@
 Modules for multi-asset analysis of finalcial time series.
 """
 
-from modfin.analysis.return_analysis import (annualized_return, expected_return,
-                                             exponencial_return, total_return)
+from modfin.analysis_.return_analysis import (annualized_return, expected_return,
+                                              exponencial_return, total_return)
 
 __all__ = [
     'annualized_return',
